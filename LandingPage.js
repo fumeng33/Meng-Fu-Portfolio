b64b668b2383cc90cea346ced8e9919b6eaf18b3
fumeng33 <67123701+fumeng33@users.noreply.github.com>
2020-11-28 19:54:53 -0600

    Add files via upload

---
+++ LandingPage.js
@@ -0,0 +1,4 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
