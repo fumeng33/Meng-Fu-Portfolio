19fcc3b8f24940ebc4042ac84a4a7e50d2f877a8
Meng Fu <Ellie@Ellies-Air.lan1>
2020-12-04 14:44:30 -0600

    Portfolio

--- LandingPage.js
+++
@@ -1,4 +0,0 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-  const navLinks = document.querySelectorAll('.nav-links li');
